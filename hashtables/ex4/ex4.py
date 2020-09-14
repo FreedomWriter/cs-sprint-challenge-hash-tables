b1696e30babc265d970a8c631f0396f88aeb5107
FreedomWriter <natalienetworks@gmail.com>
2020-09-13 21:52:00 -0500

    first test passing on ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,9 +3,33 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    hash_table = {}
+    result_with_dupes = []
+    hash_result = {}
+    # result = []
+    # print(a)
+
+    for num in a:
+        hash_table[num] = True
+
+    for key in hash_table.keys():
+        # print(key, key * -1)
+        if (key * -1) in hash_table:
+            result_with_dupes.append(abs(key))
+
+    for num in result_with_dupes:
+        if not num in hash_result:
+            hash_result[num] = 0
+        hash_result[num] += 1
+    
+    result = list(hash_result.keys())
+    
+    # print(hash_result)
+
+    # print(hash_table)
 
     return result
 
 
 if __name__ == "__main__":
-    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+    # print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
