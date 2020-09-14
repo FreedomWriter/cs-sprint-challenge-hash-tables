783525efa03c33f8975ff24eecbbc22521e08573
FreedomWriter <natalienetworks@gmail.com>
2020-09-13 22:56:39 -0500

    added comments to ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -14,17 +14,21 @@ def reconstruct_trip(tickets, length):
     
     hash_table = {}
     
+    # create entries in hash_table where the starting point is the key and the destination is the value
     for t in tickets:
-        print(t.source, t.destination)
         hash_table[t.source] = t.destination
 
     route = []
+    # add the first flight to the route by checking for the entry where the starting point is "NONE"
     route.append(hash_table["NONE"])
+    # give ourselves a pointer to use to find the next flight in our trip
     begin = hash_table["NONE"]
 
+    # loop the appropriate number of times to create a list of the expected length
     for _ in range(0, length-1):
         if begin in hash_table:
             route.append(hash_table[begin])
+            # change the value of begin so that it points to the next starting point in the sequence
             begin = hash_table[begin]
 
     return route
